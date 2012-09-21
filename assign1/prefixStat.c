81d975dc5487dc77eede662f3760ca14f3f29397
Josh Matthews <jmatth@nbcs.rutgers.edu>
2012-09-20 21:08:01 -0400

    finished flag checks

--- assign1/prefixStat.c
+++ assign1/prefixStat.c
@@ -4,49 +4,48 @@
 
 int main(int argc, const char *argv[])
 {
-	readArgs(argc, argv);
+	char action = readArgs(argc, argv);
 	return 0;
 }
 
-int readArgs(int argc, const char *argv[])
+char readArgs(int argc, const char *argv[])
 {
 	//check for too many arguments, call user an idiot
-	if (argc != 3) {
+	if (argc != 3)
+	{
 		printf("Wrong number of arguments.\n");
-		return 1;
+		return 'h';
 	}
 
-	//if there are more arguments than just
-	//commmand and file name
-	if (argc > 2) {
-		int i;
-		for (i = 1; i < argc-1; i++) {
-			//error if we have flags that are more
-			//than 2 characters long
-			if (strlen(argv[i]) > 2) {
-				printf("Long argument.\n");
-				return 0;
-			}
-			//make sure the flags are correct
-			if(argv[i][0] != '-'){
-				printf("Invalid flag. No '-'.\n");
-				return 0;
-			}
-			switch(argv[i][1])
-			{
-				case 'w' :
-					return 2;
-					break;
-				case 'l' :
-					return 3;
-					break;
-				case 'p' :
-					return 1;
-					break;
-				default :
-					return 0;
-					break;
-			}
-		}
+	//error if we have flags that are more
+	//than 2 characters long
+	if (strlen(argv[1]) > 2)
+	{
+		printf("Long argument.\n");
+		return 'h';
+	}
+
+	//make sure the flags are correct
+	if(argv[1][0] != '-')
+	{
+		printf("Invalid flag. No '-'.\n");
+		return 'h';
+	}
+
+	switch(argv[1][1])
+	{
+		case 'w' :
+			return 'w';
+			break;
+		case 'l' :
+			return 'l';
+			break;
+		case 'p' :
+			return 'p';
+			break;
+		default :
+			printf("Invalid flag.\n");
+			return 'h';
+			break;
 	}
 }
--- assign1/prefixStat.h
+++ assign1/prefixStat.h
@@ -1,2 +1,2 @@
 //check the command line arguments
-int readArgs(int argc, const char *argv[]);
+char readArgs(int argc, const char *argv[]);
