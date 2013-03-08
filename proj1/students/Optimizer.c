3e54dc09c488dedfc47459bd136fae53a9f38326
Josh Matthews <josh@jmatthews.us>
2013-03-08 09:48:26 -0500

    Starting optimizer. Code for SUB not done yet.

--- proj1/students/Optimizer.c
+++ proj1/students/Optimizer.c
@@ -25,7 +25,57 @@ int main()
 		exit(EXIT_FAILURE);
 	}
 
-	/* YOUR CODE GOES HERE */
+	/*If there are less than three instructions then there's no optimization*/
+	/*to be done.                                                           */
+	if (!head->next->next)
+	{
+		return EXIT_SUCCESS;
+	}
+
+	/*Load the first three manually.*/
+	instr1 = head;
+	instr2 = head->next;
+	instr3 = instr2->next;
+
+	/*temp storage for the result of the optimization*/
+	int opt_result;
+	while(instr3) {
+		if (instr1->opcode == LOADI && instr2->opcode == LOADI)
+		{
+			if ((instr3->field2 == instr1->field1 &&
+				 instr3->field3 == instr2->field1) ||
+				(instr3->field2 == instr2->field1 &&
+				 instr3->field3 == instr1->field1))
+			{
+				switch (instr3->opcode)
+				{
+					case ADD:
+						instr1->opcode = LOADI;
+						instr1->field1 = instr1->field1 + instr2->field1;
+						instr1->next = instr3->next;
+						free(instr2);
+						free(instr3);
+						continue;
+						break;
+					case MUL:
+						instr1->opcode = LOADI;
+						instr1->field1 = instr1->field1 * instr2->field1;
+						instr1->next = instr3->next;
+						free(instr2);
+						free(instr3);
+						continue;
+						break;
+					case SUB:
+						/*need to check the order...*/
+						/*continue;*/
+						break;
+				}
+			}
+		}
+		instr1=instr2;
+		instr2=instr3;
+		instr3=instr3->next;
+	}
 
 	PrintInstructionList(stdout, head);
 	DestroyInstructionList(head);
