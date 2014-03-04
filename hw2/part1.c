bcf99dbfd2eeb12842803cdeee2fc6df18c61061
Josh Matthews <josh@jmatthews.us>
2014-03-04 00:23:04 -0500

    Calculating the time for several interrupts in part 1.

--- hw2/part1.c
+++ hw2/part1.c
@@ -1,22 +1,37 @@
+#define ITERS 10000
 #include <stdio.h>
 #include <stdlib.h>
 #include <signal.h>
+#include <sys/time.h>
 
 void catchDivByZero()
 {
     unsigned char marker;
-    // Magic numbers divined from gdb
-    *(int*)(&marker + 69) += 8;
-    return;
+    static int counter = 0;
+    counter++;
+    if (counter > ITERS)
+    {
+        // Magic numbers divined from gdb
+        *(int*)(&marker + 69) += 8;
+    }
 }
 
 int main(int argc, char *argv[])
 {
     signal(SIGFPE, catchDivByZero);
+    int i;
     int dummy;
+    struct timeval start, end, timeDiff;
 
-    printf("before div by zero\n");
+    timeDiff.tv_sec = 0;
+    timeDiff.tv_usec = 0;
+
+    gettimeofday(&start, NULL);
     dummy = 1 / 0;
-    printf("past div by zero.\n");
+    gettimeofday(&end, NULL);
+
+    timersub(&end, &start, &timeDiff);
+    printf("%d %ld\n", ITERS, timeDiff.tv_sec * 1000000 + timeDiff.tv_usec);
+
     return 0;
 }
