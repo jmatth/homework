b6f64a2ee177a84ea11b4db4558eba025c07034c
Josh Matthews <jmatthews@twilio.com>
2015-04-09 12:57:43 -0400

    Adding lexyacc example code

---
+++ LexYacc/attr.h
@@ -0,0 +1,14 @@
+/**********************************************
+        CS415  Compilers  Project2
+**********************************************/
+
+#ifndef ATTR_H
+#define ATTR_H
+
+typedef union {char* str; int num;} tokentype;
+
+typedef struct {  
+        int mcount;
+        } multCounter;
+
+#endif
